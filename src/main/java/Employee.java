c3c2138b6950585029e1d854cc8e3fb0f1249cc4
michaelelliston <melliston@my.yearupunited.org>
2025-10-21 13:49:23 -0500

    added fields and methods for Employee class

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,2 +1,33 @@
 public class Employee {
+    private int employeeId;
+    private String name;
+    private String department;
+    private double payRate;
+    private int hoursWorked;
+
+
+    public Employee(int employeeId, String name, String department, double payRate, int hoursWorked) {
+
+    }
+
+    public double getTotalPay() {
+        double overtimePayRate = this.getPayRate() * 1.4;
+        return (getRegularHours() * getPayRate()) + (getOvertimeHours() * overtimePayRate);
+    }
+
+    public int getRegularHours() {
+        if (this.hoursWorked < 40) {
+            return hoursWorked;
+        } else {
+            return 40;
+        }
+    }
+
+    public double getPayRate() {
+        return this.payRate;
+    }
+
+    public int getOvertimeHours() {
+        return this.hoursWorked - 40;
+    }
 }
