017b08ed38816c69334ffea8e2145fb851391419
michaelelliston <melliston@my.yearupunited.org>
2025-10-21 11:59:08 -0500

    added code to test for room availability

--- src/main/java/HotelOperations.java
+++ src/main/java/HotelOperations.java
@@ -1,3 +1,31 @@
+import java.util.Scanner;
+
 public class HotelOperations {
 
+    static Scanner myScanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int menuSelection;
+
+        Room room1 = new Room(2, 80, true, true);
+
+        System.out.print("""
+                Please select a room:
+                \t1) Room 1
+                """);
+
+        menuSelection = Integer.parseInt(myScanner.nextLine());
+
+        switch (menuSelection) {
+            case 1 -> {
+                if (room1.isAvailable()) {
+                    System.out.println("This room is available! It has "
+                            + room1.getNumberOfBeds() + " beds, and costs $"
+                            + room1.getPrice() + ", would you like to rent it?");
+                } else {
+                    System.out.println("Sorry, that room is currently unavailable.");
+                }
+            }
+        }
+    }
 }
